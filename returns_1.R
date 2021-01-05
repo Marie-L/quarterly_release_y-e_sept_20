52761d36337d85d1d6898feea350240b080f9415
Marie-Louise Annan <ml_annan@hotmail.com>
2021-01-05 23:27:28 +0000

    initial commit

---
+++ returns_1.R
@@ -0,0 +1,25 @@
+install.packages("gitignore")
+library(gitignore)
+gi_fetch_templates("R")
+
+
+# load tidyverse and it's libraries
+library(tidyverse)
+library(readxl)
+library(dplyr)
+
+
+# for data cleaning
+library(janitor)
+
+# check working directory
+getwd()
+
+
+# check the excel sheet names from excel file
+readxl::excel_sheets("/Users/marie-louise/Documents/sites/quarterly_release_y:e_sept_20/returns-datasets-sep-2020.xlsx")
+
+# dataframe_name <- readxl::read_excel(file_path)
+# or - readxl::excel_sheets(file_path)
+
+raw_data_1 <- readxl::read_excel()
